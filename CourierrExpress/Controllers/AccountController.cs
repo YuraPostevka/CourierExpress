e084b8a7c0ed8bb7f3c3b99deeb73f803541e033
Yurii Postevka <ypos@softserveinc.com>
2018-02-27 19:03:11 +0200

    Added AccountController

--- CourierrExpress/Controllers/AccountController.cs
+++ CourierrExpress/Controllers/AccountController.cs
@@ -1,13 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
 namespace CourierrExpress.Controllers
 {
-    [Route("api/[controller]")]
-    public class ValuesController : Controller
+    [Route("api/account")]
+    public class AccountController : Controller
     {
         // GET api/values
         [HttpGet]
